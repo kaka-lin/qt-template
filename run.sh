fc554a879903c06d98a74a6b84b4421a9a4a7759
kaka <vn503024@gmail.com>
2022-10-19 16:30:50 +0800

    chore: update `run.sh`, fix cannot running on macOS

--- run.sh
+++ run.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-GPU=$(lspci | grep -i '.* vga .* nvidia .*')
 IMAGE_NAME="kakalin/qt:5.12.0"
-
 docker_run_params=$(cat <<-END
     --rm \
     -it \
-    -e DISPLAY=$DISPLAY \
     -e QT_X11_NO_MITSHM=1 \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v $PWD:/home/user/qt-template \
@@ -17,20 +14,24 @@ END
 
 xhost +localhost
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    GPU=$(lspci | grep -i '.* vga .* nvidia .*')
     if [[ $GPU == *' NVIDIA '* ]]; then
         # printf 'Nvidia GPU is present:  %s\n' "$GPU"
         docker run \
             --gpus all \
+            -e DISPLAY=$DISPLAY \
             $docker_run_params
     else
         docker run \
+            -e DISPLAY=$DISPLAY \
             $docker_run_params
     fi
 elif [[ "$OSTYPE" == "darwin"* ]]; then
-    DISPLAY=host.docker.internal:0
     docker run \
+        -e DISPLAY="host.docker.internal:0" \
         -e QT_QUICK_BACKEND=software \
         $docker_run_params
+    exec "$@"
 else
     echo "Haven't supported this OS Type, please check command\n and update it."
 fi
