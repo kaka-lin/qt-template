6bfde2b81c92fc44c033d67551a319989f1afeb6
Kaka <vn503024@gmail.com>
2024-06-28 03:21:12 +0800

    chore: update docker-run script file

---
+++ docker-run.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+
+XSOCK=/tmp/.X11-unix
+# change the XAUTH to your own .Xauthority file
+XAUTH=/home/user/.Xauthority
+
+DOCKER_REPO=kakalin/
+BRAND=qt
+VERSION=5.12.0
+IMAGE_NAME=${DOCKER_REPO}${BRAND}:${VERSION}
+
+# https://stackoverflow.com/questions/3162385/how-to-split-a-string-in-shell-and-get-the-last-field
+PROJECT_FOLDER=${PWD##*/}
+
+GPU_DEVICE=""
+if [[ -z $1 ]]; then
+    GPU_DEVICE="all"
+else
+    GPU_DEVICE="device=$1"
+fi
+
+# -e QT_X11_NO_MITSHM=1: Disables MIT-SHM extension for Qt applications.
+# -e XAUTHORITY=$XAUTH: Sets the XAUTHORITY environment variable to the .Xauthority file.
+# -v $XSOCK:$XSOCK: Mounts the X11 socket to the container.
+# -v $HOME/.Xauthority:$XAUTH: Mounts the .Xauthority file to the container.
+# --network=host: Shares the host network with the container.
+# --privileged: Enables privileged mode for the container.s
+docker_run_params=$(cat <<-END
+    --rm -it \
+    -e QT_X11_NO_MITSHM=1 \
+    -e XAUTHORITY=$XAUTH \
+    -v $XSOCK:$XSOCK \
+    -v $HOME/.Xauthority:$XAUTH \
+    --network=host \
+    -v $PWD:/home/user/$PROJECT_FOLDER \
+    -w /home/user/$PROJECT_FOLDER \
+    --privileged \
+    $IMAGE_NAME
+END
+)
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    GPU=$(lspci | grep -i '.* vga .* nvidia .*')
+    if [[ $GPU == *' NVIDIA '* ]]; then
+        # -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native: Sets the PulseAudio server environment variable.
+        # -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native: Mounts the PulseAudio socket to the container.
+        # -v $PWD:/home/user/$PROJECT_FOLDER: Mounts the current directory to the container.
+        docker run \
+            --gpus $GPU_DEVICE \
+            -e DISPLAY=$DISPLAY \
+            -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
+            -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
+            -v ~/.config/pulse/cookie:/root/.config/pulse/cookie \
+            $docker_run_params
+    else
+        docker run \
+            -e QT_QUICK_BACKEND=software \
+            -e DISPLAY=$DISPLAY \
+            -e PULSE_SERVER=unix:${XDG_RUNTIME_DIR}/pulse/native \
+            -v ${XDG_RUNTIME_DIR}/pulse/native:${XDG_RUNTIME_DIR}/pulse/native \
+            -v ~/.config/pulse/cookie:/root/.config/pulse/cookie \
+            $docker_run_params
+    fi
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+    docker run \
+        -e DISPLAY=host.docker.internal:0 \
+        -e QT_QUICK_BACKEND=software \
+        $docker_run_params
+else
+    echo "Haven't supported this OS Type, please check command\n and update it."
+fi
